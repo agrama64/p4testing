9c49a0d279b537f99abb36162f6f7ca2181dec19
agrama64 <aj.grama@gmail.com>
2025-01-26 15:30:08 -0600

    test script

---
+++ traffic.py
@@ -0,0 +1,19 @@
+from mininet.net import Mininet
+from mininet.cli import CLI
+from random import randint
+import time
+
+net = Mininet()
+
+h1 = net.get('h1')
+h2 = net.get('h2')
+
+for _ in range(5):
+    bandwidth = f"{randint(1, 10)}M"
+    duration = randint(5, 15)
+    print(f"Generating {bandwidth} ftraffic for {duration}s")
+    h1.cmd('iperf -s -u &')
+    h2.cmd(f'iperf -c {h1.IP()} -u -b {bandwidth} -t {duration}')
+    time.sleep(randint(2, 5))
+
+#CLI(net)
