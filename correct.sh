9c3539b0e7ef8234bba753cc452143585cc3b4a3
agrama64 <aj.grama@gmail.com>
2025-01-26 13:10:33 -0600

    fie

---
+++ correct.sh
@@ -0,0 +1,3 @@
+mv /build/basic.p4.p4info.txtpb /build/basic.p4.p4info.txt
+mv /build/switch.p4.p4info.txtpb /build/switch.p4.p4info.txt
+mv /build/load_balancer.p4.p4info.txtpb /build/load_balancer.p4.p4info.txt
