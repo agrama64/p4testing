0d765e0ad898275b19f116421b5c10aa6efe888c
agrama64 <aj.grama@gmail.com>
2025-02-28 16:45:31 -0600

    dfd

--- run_tri.sh
+++ run_tri.sh
@@ -1,5 +1,4 @@
 ./move.sh
 make clean
 make run
-./correct.sh
 sudo PATH=/home/p4/p4dev-python-venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python3 ../../utils/traffic.py -t triangle-topo/topology.json -j build/basic.json -b simple_switch_grpc
